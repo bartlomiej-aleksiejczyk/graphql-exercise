904dcf24ff2b43da10dfb2dae9d8f5149d6aeb81
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-26 02:58:00 +0200

    Add tailwind dependency

--- src/components/StyleConfiguration.tsx
+++ src/components/StyleConfiguration.tsx
@@ -1,15 +1,14 @@
 import { PrimeReactProvider } from "primereact/api";
 import { MainContent } from "./MainContent";
-import "primereact/resources/themes/bootstrap4-dark-blue/theme.css";
+import "primereact/resources/themes/lara-dark-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import "/node_modules/primeflex/primeflex.css"
-import "./index.css"
-
+import { TopSearchBar } from "./TopSearchBar";
 
 export const StyleConfiguration = () => {
 	return (
-		<PrimeReactProvider>
+		<PrimeReactProvider className="p-0 m-0">
+			<TopSearchBar/>
 			<MainContent />
 		</PrimeReactProvider>
 	);
