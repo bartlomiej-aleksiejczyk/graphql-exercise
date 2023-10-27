43074d028e393148666029ca5115b5c4a6690c0b
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-27 03:20:46 +0200

    Remove malicious comma

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ApolloProvider client={apolloClient}>
 			<App />
-		</ApolloProvider>,
+		</ApolloProvider>
 	</React.StrictMode>
 );
